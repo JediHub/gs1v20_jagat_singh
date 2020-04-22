110657547435923bbc3bd7a314c71526a452cab0
Jagat Singh <jsingh@axway.com>
2020-04-22 10:44:01 +0530

    Updates

---
+++ precache-manifest.d05c437d19871a4ff42405e8d5b989d5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b73da46854224bc1524aee71a15c60b7",
+    "url": "/gs1v20_jagat_singh/index.html"
+  },
+  {
+    "revision": "e15bbbf2561766e468ef",
+    "url": "/gs1v20_jagat_singh/static/css/main.69fa604e.chunk.css"
+  },
+  {
+    "revision": "0b94de16849526fcdfff",
+    "url": "/gs1v20_jagat_singh/static/js/2.b3353b8c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/gs1v20_jagat_singh/static/js/2.b3353b8c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e15bbbf2561766e468ef",
+    "url": "/gs1v20_jagat_singh/static/js/main.3015d88d.chunk.js"
+  },
+  {
+    "revision": "f5ab584d7f0775f07368",
+    "url": "/gs1v20_jagat_singh/static/js/runtime-main.06347d78.js"
+  },
+  {
+    "revision": "822acd399442c87a3a1e39280546ad66",
+    "url": "/gs1v20_jagat_singh/static/media/home-black.822acd39.svg"
+  },
+  {
+    "revision": "a6cfc026dc4818ab235e9676ec2cd534",
+    "url": "/gs1v20_jagat_singh/static/media/search-black.a6cfc026.svg"
+  }
+]);
